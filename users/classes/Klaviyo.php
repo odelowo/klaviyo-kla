c677a116383412f1f2a8e05aec7cd5db4650b4c1
odelowo <odelowo@live.co.uk>
2022-07-20 23:26:33 +0100

    troubleshooting dir issue

--- users/classes/Klaviyo.php
+++ users/classes/Klaviyo.php
@@ -1,5 +1,10 @@
+
 <?php
-require_once('../../vendor/autoload.php');
+/*
+Used to manage all  integrations with the Klaviyo. Extends guzzlehttp
+*/
+
+require_once __DIR__.'/../../vendor/autoload.php';
 
 class Klaviyo extends Client {
 
@@ -7,9 +12,18 @@ class Klaviyo extends Client {
   private static $publicKey = 'TMezSb';
 
   public function triggerCustomerEmailVerificationEmail($email, $properties){
+    $event = "Customer Email Verification";
+    $data = '{"token": "'.$this->publicKey.'", "event": "'.$event.'", "customer_properties": {"$email": "'.$email.'"}, "properties": {';
+
+    foreach ($properties as [$prop, $val]) {
+      $data .= '"'.$prop.'":"'.$val.'",';
+    }
+
+    $data.='}}';
+
     $response = $client->request('POST', 'https://a.klaviyo.com/api/track', [
       'form_params' => [
-        'data' => '{"token": "TMezSb", "event": "Customer Email Verification", "customer_properties": {"$email": "odelowo@live.co.uk"}, "properties": {"vericode": "XXXXXX","vericode_expiry": "YYYYYY", "vericodeURL":"URL"}}'
+        'data' => $data
       ],
       'headers' => [
         'Accept' => 'text/html',
