f5467c251c2bcea6e8d3be3e57807a7acf60f3fe
odelowo <odelowo@live.co.uk>
2022-07-20 21:30:08 +0100

    test event creation in klaviyo with hardcoded page

---
+++ tbd-dump-2.php
@@ -0,0 +1,33 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://a.klaviyo.com/api/track?api_key=pk_1128ba9f64907df9bee8a7a7f27d84e92c",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  form_params =>[
+    'data' => '{"token": "TMezSb", "event": "Customer Email Verification", "customer_properties": {"$email": "odelowo@live.co.uk"}, "properties": {"vericode": "XXXXXX","vericode_expiry": "YYYYYY"}}'
+  ],
+  CURLOPT_HTTPHEADER => [
+    "Accept: text/html",
+    "Content-Type: application/x-www-form-urlencoded"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+
+?>
