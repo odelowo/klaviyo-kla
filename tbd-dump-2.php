c3b598b630d8c49754af11ae484732388ffc5d07
odelowo <odelowo@live.co.uk>
2022-07-20 21:33:31 +0100

    curl fix

--- tbd-dump-2.php
+++ tbd-dump-2.php
@@ -10,7 +10,7 @@ curl_setopt_array($curl, [
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  form_params =>[
+  DATA =>[
     'data' => '{"token": "TMezSb", "event": "Customer Email Verification", "customer_properties": {"$email": "odelowo@live.co.uk"}, "properties": {"vericode": "XXXXXX","vericode_expiry": "YYYYYY"}}'
   ],
   CURLOPT_HTTPHEADER => [
