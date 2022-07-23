7be12ff888e2194f417fd2a5432df1ad56e5c5b7
odelowo <odelowo@live.co.uk>
2022-07-24 00:39:00 +0100

    update js

--- platform/quiz-details.php
+++ platform/quiz-details.php
@@ -23,7 +23,22 @@ error_reporting(E_ALL);
 
 
 ?>
+<script>
+//add row button logic
+var rowNum = 0; //initialise to 0
 
+function addRow(frm) { //on add row click
+  rowNum ++; //increment by 1
+  var row = '<p id="rowNum'+rowNum+'"><input type="text" name="question[]" placeholder="Question"  value="'+frm.add_question.value+'"/> <input type="button" value="Remove" onclick="removeRow('+rowNum+');"></p>';
+  jQuery('#itemRows').append(row); //append above code to the bottom
+  frm.add_question.value = ''; //reset question to ""
+}
+
+function removeRow(rnum) { //remove row
+  jQuery('#rowNum'+rnum).remove();
+}
+
+</script>
      <div class="ObjectivesTab__Container-sc-1el61mo-0 coNUBW">
         <div class="ObjectivesTab__DashboardContainer-sc-1el61mo-1 khhmlX">
            <div class="Box__StyledBox-sc-16nrscc-0 izgCeZ HeroCardWrapper__StyledBox-sc-1monayj-1 hiaYEj">
@@ -38,22 +53,7 @@ error_reporting(E_ALL);
            </div>
         </div>
      </div>
-    <script>
-    //add row button logic
-    var rowNum = 0; //initialise to 0
 
-    function addRow(frm) { //on add row click
-      rowNum ++; //increment by 1
-      var row = '<p id="rowNum'+rowNum+'"><input type="text" name="question[]" placeholder="Question"  value="'+frm.add_question.value+'"/> <input type="button" value="Remove" onclick="removeRow('+rowNum+');"></p>';
-      jQuery('#itemRows').append(row); //append above code to the bottom
-      frm.add_question.value = ''; //reset question to ""
-    }
-
-    function removeRow(rnum) { //remove row 
-      jQuery('#rowNum'+rnum).remove();
-    }
-
-    </script>
  <?php
  require_once('includes/footer.php');
  ?>
