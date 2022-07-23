6baabe05c9b076eb77d27b504cbf6c8e4cceb64d
odelowo <odelowo@live.co.uk>
2022-07-23 17:09:05 +0100

    create dashboard/platform page

---
+++ platform/includes/footer.php
@@ -0,0 +1,43 @@
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!-- <script type="text/javascript" src="js/vendor-initial.a5794c5f6f33a0678b1b.js" defer=""></script> -->
+
+<script>
+$(function() { // Dropdown toggle for account and notifications 
+$('.dropdown-toggle').click(function() {
+var elm = $(this).attr('aria-controls');
+$(this).attr('aria-expanded','true');
+$('#'+elm).slideToggle();
+});
+
+// $(document).click(function(e)
+// {
+// var target = e.target;
+// if (!$(target).is('#kl-notification-list') && !$(target).is('#kl-user-menu'))
+//   {
+//     $('#kl-notification-list').hide();
+//     $('#kl-user-menu').hide();
+//   }
+//   //{ $('.dropdown').slideUp(); }
+// });
+});
+
+</script>
+
+
+<link rel="stylesheet" href="https://use.fortawesome.com/kits/daab876d/publications/120557/woff2.css" media="all">
+<link rel="stylesheet" href="https://use.fortawesome.com/kits/34cf58b5/publications/120554/woff2.css" media="all">
+<link rel="stylesheet" href="https://use.fortawesome.com/kits/dc0e22bb/publications/120556/woff2.css" media="all">
+<link rel="stylesheet" href="https://use.fortawesome.com/kits/d537f022/publications/119773/woff2.css" media="all">
+<link rel="stylesheet" href="https://use.fortawesome.com/kits/e7f923c5/publications/120438/woff2.css" media="all">
+<link rel="stylesheet" href="https://use.fortawesome.com/kits/6480572f/publications/120437/woff2.css" media="all">
+<script src="https://cdn.sift.com/s.js"></script>
+<div class="ReactModalPortal"></div>
+<div class="ReactModalPortal"></div>
+<div class="ReactModalPortal"></div>
+</body>
+</html>
